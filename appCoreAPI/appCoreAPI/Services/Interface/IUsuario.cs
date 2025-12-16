d4c4e3562b539fd6c408f79e7c9e295373dff10e
Jhuli Trujillo <jhulitrujillomeza@gmail.com>
2025-12-15 21:18:29 -0500

    agregar usuario

---
+++ appCoreAPI/appCoreAPI/Services/Interface/IUsuario.cs
@@ -0,0 +1,18 @@
+﻿using appCoreAPI.Models;
+
+namespace appCoreAPI.Services.Interface
+{
+    public interface IUsuario
+    {
+        IEnumerable<Usuario> GetAllUsuarios();
+
+        string RegistrarUsuario(Usuario usuario);
+        string ActualizarUsuario(Usuario usuario);
+        Usuario GetUsuarioById(int id);
+        int EliminarUsuario(int idUsuario);
+
+        
+        Usuario Login(string correo, string clave);
+
+    }
+}
